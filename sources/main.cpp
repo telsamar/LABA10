6f2f46485c59291a50001071feb61283fba64d95
telsamar <Stolya2000@mail.ru>
2020-05-22 15:05:39 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright 2020 <boikov3>
+﻿// Copyright 2020 <telsamar>
 #include <DBHashCreator.hpp>
 #include <constants.hpp>
 #include <logs.hpp>
@@ -44,83 +44,3 @@ int main(int argc, char **argv) {
     db.startThreads();
     return 0;
 }
-/*
-#include <cstdio>
-#include <iostream>
-#include <string>
-
-#include "rocksdb/db.h"
-#include "rocksdb/options.h"
-#include "rocksdb/slice.h"
-
-std::string kDBPath = "/home/ivan/rocksdb_simple_example";
-
-int main() {
-  rocksdb::DB* db;
-  rocksdb::Options options;
-  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
-  //options.IncreaseParallelism();
-  //options.OptimizeLevelStyleCompaction();
-  // create the DB if it's not already present
-  options.create_if_missing = true;
-
-  // open DB
-  rocksdb::Status s = rocksdb::DB::Open(options, kDBPath, &db);
-  if(!s.ok()) std::cerr << s.ToString() << std::endl;
-
-  // Put key-value
-  s = db->Put(rocksdb::WriteOptions(), "key1", "value");
-  assert(s.ok());
-  std::string value;
-  // get value
-  s = db->Get(rocksdb::ReadOptions(), "key1", &value);
-  assert(s.ok());
-  std::cout << value;
-  assert(value == "value");
-
-  // atomically apply a set of updates
-  {
-    rocksdb::WriteBatch batch;
-    batch.Delete("key1");
-    batch.Put("key2", value);
-    s = db->Write(rocksdb::WriteOptions(), &batch);
-  }
-
-  s = db->Get(rocksdb::ReadOptions(), "key1", &value);
-  assert(s.IsNotFound());
-
-  db->Get(rocksdb::ReadOptions(), "key2", &value);
-  assert(value == "value");
-
-  {
-    rocksdb::PinnableSlice pinnable_val;
-    db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-  }
-
-  {
-    std::string string_val;
-    // If it cannot pin the value, it copies the value to its internal buffer.
-    // The intenral buffer could be set during construction.
-    rocksdb::PinnableSlice pinnable_val(&string_val);
-    db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-    // If the value is not pinned, the internal buffer must have the value.
-    assert(pinnable_val.IsPinned() || string_val == "value");
-  }
-
-  rocksdb::PinnableSlice pinnable_val;
-  db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key1", &pinnable_val);
-  assert(s.IsNotFound());
-  // Reset PinnableSlice after each use and before each reuse
-  pinnable_val.Reset();
-  db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-  assert(pinnable_val == "value");
-  pinnable_val.Reset();
-  // The Slice pointed by pinnable_val is not valid after this point
-
-  delete db;
-
-  return 0;
-}
- */
